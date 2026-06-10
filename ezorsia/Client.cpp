0b3e45bd41c508c5add59f52d8d9268312e95457
DESKTOP-N04BE05\HomCrazy <610861816@qq.com>
2026-06-10 10:17:58 +0800

    fix：dll注入强升版本号

--- ezorsia/Client.cpp
+++ ezorsia/Client.cpp
@@ -141,7 +141,7 @@ void Client::UpdateGameStartup() {
 	// 删除角色时客户端只允许 PIC 状态为 1；服务端关闭 PIC 会下发 2，这里放过本地误判。
 	Memory::WriteByte(0x005F7CA6, 0xEB);
 
-	unsigned char bdClientSignature[] = { 0x68, 0x42, 0x44, 0x11, 0x00 };
+	unsigned char bdClientSignature[] = { 0x68, 0x42, 0x44, 0x12, 0x00 };
 	Memory::WriteByteArray(0x005F6BCD, bdClientSignature, sizeof(bdClientSignature));
 
 	//optional non-resolution related stuff
